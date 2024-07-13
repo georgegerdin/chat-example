7634a7fb3d1f6d83bfe19c0430264df5be70e10f
George Gerdin <georgegerdin@gmail.com>
2024-07-13 08:52:25 +0200

    Change the signal class to use std::function instead of boost::signals2

--- include/chat_example/signal.hh
+++ include/chat_example/signal.hh
@@ -2,45 +2,62 @@
 #pragma once
 
 #include <boost/asio.hpp>
-#include <boost/signals2.hpp>
+#include <functional>
+#include <optional>
+#include <mutex>
 
 template <typename... Args>
 class Signal {
 public:
-    using SignalType = boost::signals2::signal<void(Args...)>;
-    using SlotType = typename SignalType::slot_type;
+    using SlotType = std::function<void(Args...)>;
 
     void connect(const SlotType& slot, boost::asio::io_service* io_service = nullptr) {
+        std::lock_guard<std::mutex> lock(m_mutex);
         if (io_service) {
-            m_signal.connect([io_service, slot](Args... args) {
+            m_slot = [io_service, slot](Args... args) {
                 io_service->post([slot, args...]() {
                     slot(args...);
                 });
-            });
+            };
         } else {
-            m_signal.connect(slot);
+            m_slot = slot;
         }
     }
 
     void connect(const SlotType& slot, std::shared_ptr<boost::asio::io_service> io_service) {
+        std::lock_guard<std::mutex> lock(m_mutex);
         if (io_service) {
             std::weak_ptr<boost::asio::io_service> weak_io_service = io_service;
-            m_signal.connect([weak_io_service, slot](Args... args) {
+            m_slot = [weak_io_service, slot](Args... args) {
                 if (auto io_service = weak_io_service.lock()) {
                     io_service->post([slot, args...]() {
                         slot(args...);
                     });
                 }
-            });
+            };
         } else {
-            m_signal.connect(slot);
+            m_slot = slot;
         }
     }
 
     void emit(Args... args) {
-        m_signal(args...);
+        std::lock_guard<std::mutex> lock(m_mutex);
+        if (m_slot) {
+            (*m_slot)(args...);
+        }
+    }
+
+    void disconnect() {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_slot.reset();
+    }
+
+    bool connected() const {
+        std::lock_guard<std::mutex> lock(m_mutex);
+        return m_slot.has_value();
     }
 
 private:
-    SignalType m_signal;
+    std::optional<SlotType> m_slot;
+    mutable std::mutex m_mutex;
 };
